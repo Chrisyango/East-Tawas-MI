02ebe6aea2c09b1724a636615ccd3196a67e68f2
Chris Yang <cyang287@yahoo.com>
2019-03-26 12:00:25 -0400

    finished html markup

---
+++ index.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<title>East Tawas, MI</title>
+	<meta name="description" content="">
+	<meta name="robots" content="index, follow">
+
+	<link rel="stylesheet" href="_assets_/plugins/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="_assets_/fonts/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="_assets_/css/animate.min.css">
+	<link rel="stylesheet" href="_assets_/plugins/owl.carousel/owl-carousel/owl.carousel.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:500,600,900|Roboto:700">
+	<link rel="stylesheet" href="_assets_/css/layout.css">
+
+	<link rel="shortcut icon" href="_assets_/images/favicon.ico">
+	<link rel="apple-touch-icon" href="_assets_/images/touch-icon-iphone.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="_assets_/images/touch-icon-ipad.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="_assets_/images/touch-icon-iphone4.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="_assets_/images/touch-icon-ipad2.png">
+
+	<!--[if !IE]><!-->
+	<link rel="stylesheet" href="_assets_/plugins/add-to-homescreen/style/addtohomescreen.css">
+	<script src="_assets_/plugins/add-to-homescreen/src/addtohomescreen.min.js"></script>
+	<script>addToHomescreen();</script>
+	<!--<![endif]-->
+
+	<!-- Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+<body id="homepage">
+
+<header class="clearfix">
+	<a href="#main" id="skip" tabindex="0">SKIP TO CONTENT</a>
+
+	<div class="container">
+		<a href="./" id="logo">
+			<img src="./_assets_/images/logo.png" alt="Logo">
+		</a><!-- /.logo -->
+
+		<div id="toggles" class="hidden-lg hidden-md">
+			<div tabindex="0" id="search-toggle" class="fa fa-search"></div>
+			<div tabindex="0" id="nav-toggle" class="fa fa-bars"></div>	
+		</div><!-- /#toggles -->
+
+		<nav>
+			<ul id="nav">
+				<li><a href="./">home</a></li>
+				<li><a href="./">government</a></li>
+				<li><a href="./">residents</a></li>
+				<li>
+					<a href="./">businesses</a>
+					<ul>
+						<li><a href="./">this is a link</a></li>
+						<li>
+							<a href="./">this is a link</a>
+							<ul>
+								<li><a href="./">this is a link</a></li>
+								<li><a href="./">this is a link</a></li>
+								<li><a href="./">this is a link</a></li>
+								<li><a href="./">this is a link</a></li>
+							</ul>
+						</li>
+						<li><a href="./">this is a link</a></li>
+						<li><a href="./">this is a link</a></li>
+					</ul>
+				</li>
+				<li><a href="./">businesses</a></li>
+				<li><a href="./">visitors</a></li>
+				<li><a href="./">how do i...</a></li>
+			</ul> <!-- /#nav -->
+		</nav>
+
+		<div class="social-links hidden-sm hidden-xs">
+			<a href="./" class="fa fa-facebook"></a>
+			<a href="./" class="fa fa-twitter"></a>
+		</div><!-- /.social-links -->
+
+		<div id="search">
+			<form class="search-form" method="get" action="search.php">
+				<label class="fa fa-search" for="search-input"></label>
+				<input name="q" class="form-control search-input" placeholder="SEARCH HERE" type="search" id="search-input">
+				<button><i class="fa fa-search"></i></button>
+			</form><!-- /.search-form -->
+		</div><!-- /#search -->
+	</div><!-- /.container -->
+</header><!-- /.clearfix -->
+
+<section id="slider" style="display:none;">
+	<ul class="bxslider">
+		<li style="background: url('./_assets_/images/hero.jpg') center no-repeat;background-size: cover;"></li>
+	</ul><!-- /.bxslider -->
+
+	<div class="container">
+		<h1><span>welcome to</span>east tawas<span>michigan</span></h1>
+	</div><!-- /.container -->
+</section><!-- /#slider -->
+
+<main id="main">
+	<section id="quick-links">
+		<div class="container">
+			<div id="quick-links-wrapper" class="owl-carousel">
+				<a href="./" class="quick-link">
+					<i class="fa fa-credit-card"></i>
+					<span>bill pay</span>
+				</a><!-- /.quick-links -->
+				<a href="./" class="quick-link">
+					<i class="fa fa-folder-open"></i>
+					<span>forms & documents</span>
+				</a><!-- /.quick-links -->
+				<a href="./" class="quick-link">
+					<i class="fa fa-users"></i>
+					<span>get connected</span>
+				</a><!-- /.quick-links -->
+				<a href="./" class="quick-link">
+					<i class="fa fa-envelope"></i>
+					<span>contact us</span>
+				</a><!-- /.quick-links -->
+				<a href="./" class="quick-link">
+					<i class="fa fa-clipboard-list"></i>
+					<span>code of ordinances</span>
+				</a><!-- /.quick-links -->
+			</div><!-- /#quick-links-wrapper.owl-carousel -->
+		</div><!-- /.container -->
+	</section><!-- /#quick-links -->
+
+	<section id="content">
+		<div id="content-bg" style="background:url('./_assets_/images/content-bg.jpg) center no-repeat;background-size:cover;"></div>
+		<section id="news">
+			<div class="container">
+				<h2>recent news</h2>
+				<div id="news-links">
+					<div class="news-link">
+						<div class="news-img" style="background:url('./_assets_/images/news1.jpg') center no-repeat;background-size:cover;"></div>
+						<span>feb. 18, 2019</span>
+						<h3>news post title cras egestas ligula purus</h3>
+						<a href="./">read more</a>
+					</div><!-- /.news-link -->
+					<div class="news-link">
+						<div class="news-img" style="background:url('./_assets_/images/news1.jpg') center no-repeat;background-size:cover;"></div>
+						<span>feb. 14, 2019</span>
+						<h3>consectetur ligula bibendum tellus malesuada</h3>
+						<a href="./">read more</a>
+					</div><!-- /.news-link -->
+					<div class="news-link">
+						<div class="news-img" style="background:url('./_assets_/images/news1.jpg') center no-repeat;background-size:cover;"></div>
+						<span>feb. 7, 2019</span>
+						<h3>cras ipsum inceptos ridiculus egestas</h3>
+						<a href="./">read more</a>
+					</div><!-- /.news-link -->
+				</div><!-- /#news-links -->
+				<a href="./" class="btn">see all news</a>
+			</div><!-- /.contianer -->
+		</section><!-- /#news -->
+
+		<section id="events">
+			<div class="container">
+				<h2>upcoming events</h2>
+				<img src="./_assets_/images/events.png" alt="Events">
+				<a href="./" class="btn">see all events</a>
+			</div><!-- /.container -->
+		</section><!-- /#events -->
+	</section><!-- /#content -->
+</main>
+
+<footer>
+	<section id="top-footer">
+		<div class="container">
+			<section id="contact-info">
+				<span>&copy; 2019 City of East Tawas, MI</span>
+				<span>Phone: (989) 362-6161</span>
+				<span>Fax: (989) 362-6736</span>
+				<div>760 Newman Street, PO Box 672, East Tawas, MI 48730</div>
+			</section><!-- /#contact-info -->
+
+			<div class="social-links hidden-sm hidden-xs">
+				<a href="./" class="fa fa-facebook"></a>
+				<a href="./" class="fa fa-twitter"></a>
+				<a href="./" class="fa fa-instagram"></a>
+			</div><!-- /.social-links -->
+		</div><!-- /.container -->
+	</section><!-- /#top-footer.clearfix -->
+		
+	<section id="revize-logo">
+		<div class="container">
+			<span>Powered by <a href="https://www.revize.com/" id="revize-logo">revize.</a> - The Government Website Experts.</span>
+			<a href="./">login</a>
+		</div><!-- /.container -->
+	</section><!-- /#revize-logo -->
+</footer>
+
+<div class="alert alert-dismissible floating-alert fade in os-animation" data-os-animation="fadeInUpBig" data-os-animation-delay="2s" role="alert">
+	<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	<h4>Alert Title 30 characters Max</h4>
+	<p>Max 120 characters Max 120 characters Max 120 characters Max 120 characters Max 120 characters Max 120 characters Max 120</p>
+	<p><a href="#" class="btn">Read More</a></p>
+</div><!-- /.alert -->
+
+
+<!-- Share widget make into an include file -->
+<button type="button" class="share-btn floating-share-btn" data-toggle="modal" data-target="#shareModal">
+	<i class="fa fa-share-alt"></i>
+	<span>Share</span>
+</button>
+
+<div class="modal fade" id="shareModal" tabindex="-1" role="dialog" aria-labelledby="shareModal">
+	<div class="modal-dialog modal-lg" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title" id="myModalLabel">Share this page</h4>
+			</div>
+			<div class="modal-body">
+				<div class="copylink">
+					<p>Copy and paste this code into your website.</p>
+					<pre>&lt;a href="<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>">Your Link Name&lt;/a&gt;</pre>
+				</div><!-- /.copylink -->
+				<div class="share-btns">
+					<p>Share this page on your favorite Social network</p>
+					<div class="row">
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-facebook" onclick="return !window.open(this.href, 'facebook ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-facebook"></i> Facebook
+							</a>
+						</div>
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.twitter.com/home?status=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-twitter" onclick="return !window.open(this.href, 'twitter ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-twitter"></i> Twitter
+							</a>
+						</div>
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.plus.google.com/share?url=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-google" onclick="return !window.open(this.href, 'google ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-google-plus"></i> Google Plus
+							</a>
+						</div>
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.reddit.com/submit?url=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-reddit" onclick="return !window.open(this.href, 'redit ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-reddit"></i> Reddit
+							</a>
+						</div>
+					</div><!-- /.row -->
+				</div><!-- /.share-btns -->
+				<button type="button" class="btn btn-success btn-lg" data-dismiss="modal">Close</button>
+			</div><!-- /.modal-body -->
+		</div>
+	</div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<!-- Share widget make into an include file -->
+
+
+<script src="_assets_/js/jquery.min.js"></script>
+<script src="_assets_/plugins/modernizr/modernizr.custom.js"></script>
+<script src="_assets_/plugins/owl.carousel/owl-carousel/owl.carousel.min.js"></script>
+<script src="_assets_/plugins/jquery.bxslider/jquery.bxslider.min.js"></script>
+<script src="_assets_/plugins/bootstrap/js/bootstrap.min.js"></script>
+<script src="_assets_/plugins/twitter/js/jquery.tweet.min.js"></script>
+<script src="_assets_/plugins/instafeed/instafeed.min.js"></script>
+<script src="_assets_/plugins/sticky/jquery.sticky.js"></script>
+<script src="_assets_/plugins/matchHeight/dist/jquery.matchHeight-min.js"></script>
+<script src="_assets_/plugins/revizeWeather/js/revizeWeather.min.js"></script>
+<script src="_assets_/plugins/waypoints/waypoints.min.js"></script>
+<script src="_assets_/js/scripts.js"></script>
+
+</body>
+</html>
